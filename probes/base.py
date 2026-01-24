0aa56c588a61a2117da61babc081025c48ffa36e
BarakOr1 <164471955+BarakOr1@users.noreply.github.com>
2026-01-24 14:06:21 +0200

    Update base.py

--- probes/base.py
+++ probes/base.py
@@ -1,5 +1,5 @@
 """
-Base interface for measurement probes.
+Base interface for external measurement probes.
 """
 
 
@@ -8,12 +8,17 @@ class Probe:
     Abstract measurement probe.
 
     A probe evaluates a proposed model state using information
-    external to the training objective.
+    external to the training objective (e.g. validation probes).
     """
 
     def evaluate(self, model):
         """
-        Evaluate the proposed model state.
+        Evaluate the current model state.
+
+        Parameters
+        ----------
+        model : torch.nn.Module
+            Model to be evaluated.
 
         Returns
         -------
@@ -21,4 +26,3 @@ class Probe:
             Scalar measurement value.
         """
         raise NotImplementedError
-
